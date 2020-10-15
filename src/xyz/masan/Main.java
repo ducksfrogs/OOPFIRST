bdce482d93704ad662f8110e75b304687312165f
ma <ma_yamaki@yahoo.com>
2020-10-15 21:31:28 +0900

    oop first

--- src/xyz/masan/Main.java
+++ src/xyz/masan/Main.java
@@ -5,7 +5,20 @@ public class Main {
         Hero h = new Hero();
         h.name = "Minato";
         h.hp = 100;
+
+        Matango m1 = new Matango();
+        m1.hp = 50;
+        m1.suffix = 'A';
+
+        Matango m2 = new Matango();
+        m2.hp = 48;
+        m2.suffix = 'B';
+
         System.out.println("Breave " + h.name + " boarn");
+
+        h.slip();
+        m1.run();
+        m2.run();
 
     }
 }
---
+++ src/xyz/masan/Matango.java
@@ -0,0 +1,11 @@
+package xyz.masan;
+
+public class Matango {
+    int hp;
+    final int LEVEL = 10;
+    char suffix;
+
+    void run(){
+        System.out.println("beast " + this.suffix + " run away");
+    }
+}
